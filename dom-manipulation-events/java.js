f3a074733fcae54e0f3dea26ecd3b8744844b6a6
RhazzXIX <jm21tonio@gmail.com>
2022-11-09 16:40:24 +0800

    Learn DOM Manipulation and Events java
    Created a new directory for the lesson.
    Created new index.html, java.js, and style.css that will be used in
    the lesson.
    Inside the files are the things I learned like, creating new elements
    for the html file and styling them, from the java file.
    Refreshed my learnings in linking the java and css files to the html
    files.
    Started learning about events.

---
+++ dom-manipulation-events/java.js
@@ -0,0 +1,52 @@
+const container = document.querySelector('#container');
+
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'Hello Upskilling World!!!';
+
+const para = document.createElement('p');
+para.setAttribute('style', 'color: red;')
+para.textContent = 'Hey I\'m red!'
+
+const title = document.createElement('h3');
+title.setAttribute('style', 'color: blue;');
+title.textContent = 'I\'m a blue h3!'
+
+const subDiv = document.createElement('div');
+subDiv.setAttribute('style', 'padding: 6px; border: solid black; background: pink;');
+
+const subTitle = document.createElement('h1');
+subTitle.textContent = 'Im in a div';
+
+const subPara = document.createElement('p');
+subPara.textContent = 'ME TOO';
+
+subDiv.appendChild(subTitle);
+subDiv.appendChild(subPara);
+
+
+
+
+container.appendChild(content);
+container.appendChild(para);
+container.insertBefore(title, content);
+container.appendChild(subDiv);
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert('Hello Upskilling World!!!');
+const butn = document.querySelector('#butn');
+butn.addEventListener('click', () => {
+    alert('Hello Upskilling World!!!');
+});
+
+function alertFunction() {
+    alert('YAY! I DID IT!');
+}
+
+btn.onclick = alertFunction;
+btn.addEventListener('click', alertFunction);
+const bttn = document.querySelector('#bttn');
+bttn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
