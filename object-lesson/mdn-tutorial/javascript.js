5d8efcf3523fe1f7dff7299a760715d30db7b76a
RhazzXIX <jm21tonio@gmail.com>
2022-11-17 13:12:42 +0800

    Finish all MDN lessons and tasks

--- object-lesson/mdn-tutorial/javascript.js
+++ object-lesson/mdn-tutorial/javascript.js
@@ -80,10 +80,116 @@
 
 //INTRODUCING CONSTRUCTORS
 
-function createPerson(name) {
-    const obj = {};
-    obj.name = name;
-    obj.introduceSelf = function () {
-        console.log (`Hi! I'm ${this.name}`)
-    }
+// function createPerson(name) {
+//     const obj = {};
+//     obj.name = name;
+//     obj.introduceSelf = function () {
+//         console.log (`Hi! I'm ${this.name}`)
+//     };
+//     return obj;
+// }
+
+// const salva = createPerson('Salva');
+// const frankie = createPerson('Frankie');
+
+// function Person(name) {
+//     this.name = name;
+//     this.introduceSelf = function () {
+//         console.log(`Hi! I'm ${this.name}.`);
+//     };
+// }
+
+// const salva = new Person ('Salva');
+// const frankie = new Person ('frankie')
+
+//YOU'VE BEEN USING OBJECTS ALL ALONG#####
+
+
+// myString.split(",");
+
+// const myDiv = document.createElement("div");
+// const myVideo = document.querySelector("video");
+
+// const myNotification = new Notification("Hello!");
+
+
+//TEST YOUR SKILLS: OBJECT BASICS####
+
+//Object basics 1
+
+
+// const cat = {
+//     name : 'Bertie',
+//     breed : 'Cymric',
+//     color : 'white',
+//     greeting: function() {
+//       console.log('Meow!');
+//     }
+// }
+
+// const catName = cat['name'];
+// cat.greeting();
+// cat.color = 'black';
+
+//Object basics 2
+
+// let bandInfo;
+
+// const band = {
+//     name: 'Panic at the Disco!',
+//     nationality: 'American',
+//     genre: 'Pop',
+//     members: ['Brendon Urie', 'Ryan Ross', 'Spencer Smith'],
+//     formed: '2005',
+//     split: '2009',
+//     albums: {
+//         name: ['High Hopes', 'A Fever You Can\'t Sweat Out'],
+//         released: ['2018', '2005']
+//     }
+// }
+
+// bandInfo = `The ${band.name} is an all ${band.nationality} ${band.genre} group. Their members ${band.members[0]}, ${band.members[1]},
+// and ${band.members[2]}, formed their group last ${band.formed} and release their album named "${band.albums.name[1]}". They split up last ${band.split} and
+// ${band.members[0]} was the only one left. At ${band.albums.released[0]} he released a solo album named "${band.albums.name[0]}" which became a hit!`;
+
+//Object basics 3
+
+// const cat = {
+//     name : 'Bertie',
+//     breed : 'Cymric',
+//     color : 'white',
+//     greeting: function() {
+//       console.log(`Hello, said ${this.name} the ${this.breed}`);
+//     }
+// };
+
+// const cat2 = {
+//     name : 'Miming',
+//     breed : 'Catpin',
+//     color : 'Zebra',
+//     greeting: function() {
+//       console.log(`Hello, said ${this.name} the ${this.breed}`);
+//     }
+// };
+
+//Object basics 4
+
+const cat = {
+    name : 'Bertie',
+    breed : 'Cymric',
+    color : 'white',
 }
+  
+  const cat2 = {
+    name : 'Elfie',
+    breed : 'Aphrodite Giant',
+    color : 'ginger',
+}
+
+function addGreeting (obj) {
+    obj.greeting = function() {console.log(`Hello, said ${this.name} the ${this.breed}`)};
+}
+  
+addGreeting(cat);
+addGreeting(cat2);
+      
